84ebed9c92b49fa8e7ed475c7032c72325a120e6
Dylan Wetterling <dwetterling@my.wctc.edu>
2022-01-31 13:59:13 -0600

    Write data to text file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace SleepData
 {
@@ -33,6 +34,8 @@ namespace SleepData
                 // random number generator
                 Random rnd = new Random();
 
+                // create file
+                StreamWriter sw = new StreamWriter("data.txt");
                 // loop for the desired # of weeks
                 while (dataDate < dataEndDate)
                 {
@@ -43,12 +46,15 @@ namespace SleepData
                         // generate random number of hours slept between 4-12 (inclusive)
                         hours[i] = rnd.Next(4, 13);
                     }
+                    
                     // M/d/yyyy,#|#|#|#|#|#|#
-                    Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                   // Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                   //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
                     // add 1 week to date
                     dataDate = dataDate.AddDays(7);
                 }
-
+                    sw.Close();
             }
             else if (resp == "2")
             {
