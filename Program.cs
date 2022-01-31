b63b4517551837ad656e2d1171a74b5668c8bbc1
Dylan Wetterling <dwetterling@my.wctc.edu>
2022-01-31 13:48:32 -0600

    Input number of weeks of data

--- Program.cs
+++ Program.cs
@@ -6,10 +6,29 @@ namespace SleepData
     {
         static void Main(string[] args)
         {
-             Console.WriteLine("Enter 1 to create data file.");
+            // ask for input
+            Console.WriteLine("Enter 1 to create data file.");
             Console.WriteLine("Enter 2 to parse data.");
             Console.WriteLine("Enter anything else to quit.");
             // input response
+            string resp = Console.ReadLine();
+
+            if (resp == "1")
+            {
+                // TODO: create data file
+                 // create data file
+
+                 // ask a question
+                Console.WriteLine("How many weeks of data is needed?");
+                // input the response (convert to int)
+                int weeks = int.Parse(Console.ReadLine());
+
+            }
+            else if (resp == "2")
+            {
+                // TODO: parse data file
+
+            }
         }
     }
 }
